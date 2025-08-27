87f585a6473dda2c3af3c155de18b779821d8251
ranjanrahul200 <ranjanrahul200@gmail.com>
2025-08-27 20:35:18 +0530

    Create firstprogram.cs

---
+++ firstprogram.cs
@@ -0,0 +1,9 @@
+class Program
+{
+  public void Main(String [] args)
+  {
+    string variable = "Rahul";
+    string Variable2 = "Ranjan";
+    Console.WriteLine("{variable} is {Variable2}");
+  }
+}
